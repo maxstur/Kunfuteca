05547c4950b0ec8bc25ea9042186148c18c668a5
Maximiliano Rodríguez <maarashu@gmail.com>
2024-02-27 23:41:51 -0300

    Recupero commit entrega chat

---
+++ src/dbConnect/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+module.exports = connectDB;
